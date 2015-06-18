1bf254a450b7e096604700e8752cd639b1079fcd
ksoma <mikilab@mikilab.local>
2015-06-19 02:19:48 +0900

    first commit

---
+++ RSSReader/tmp.swift
@@ -0,0 +1,9 @@
+//
+//  tmp.swift
+//  RSSReader
+//
+//  Created by mikilab on 2015/06/19.
+//  Copyright (c) 2015年 susieyy. All rights reserved.
+//
+
+import Foundation
---
+++ view2ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  view2ViewController.swift
+//  RSSReader
+//
+//  Created by mikilab on 2015/06/19.
+// //
+
+import UIKit
+
+class view2ViewController: UIViewController {
+
+    @IBAction func syuyoubutton(sender: AnyObject) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
